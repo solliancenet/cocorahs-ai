651e571cb2aabd68a441e5a55978a3730f3babb2
Gabriel Gheorghescu <gabriel.gheorghescu@genisoft.eu>
2020-10-19 16:05:10 +0300

    Display year in a selector for scale bars

--- portal/src/app/app.component.ts
+++ portal/src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent {
   chartVisibility = [false, false, false];
   visibilityText = ['Show', 'Show', 'Show', 'Show'];
 
+  public selectedYear = 2017;
+  public years = [2017, 2018, 2019];
   changeChartVisibility(chartIndex: number) {
     this.chartVisibility[chartIndex] = !this.chartVisibility[chartIndex];
     this.visibilityText[chartIndex] = (this.chartVisibility[chartIndex] === false) ? 'Show' : 'Hide';
